758b31c85034fb4ae134e2bb72a25340607ebac6
DhouhaNBIBA <70754078+DhouhaNBIBA@users.noreply.github.com>
2021-01-01 11:00:25 +0100

    Add files via upload

---
+++ routes/contactUs.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+router.get('/contact',(req,res)=>{
+    res.sendFile(path.join(process.cwd () +'/public/contact.html'));
+  });
+module.exports = router;
